211a0d6102a04a3078b8585d2404ee1260d3d6ee
Massatrio16 <146039181+Massatrio16@users.noreply.github.com>
2024-05-02 12:07:50 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,10 +5,26 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-
-
-
 LOCAL_PATH := device/itel/A666LN
+
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+PRODUCT_PACKAGES_DEBUG += \
+BootControll
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# A/B support
+AB_OTA_UPDATER := true
+
+
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -18,36 +34,25 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-service
-
-PRODUCT_PACKAGES += \
+vendor.sprd.hardware.boot@1.2-impl \
+vendor.sprd.hardware.boot@1.2-service 
+    #android.hardware.boot@1.0-impl \
+  #  android.hardware.boot@1.0-service
+  
+  PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service \
     libhealthd.$(PRODUCT_PLATFORM)
-TW_LOAD_VENDOR_MODULES := "goodix_fp.ko  modules.load modules.load.recovery msm_drm.ko sprd_audcp_boot.ko  fpsensor_fp.ko sprd_sensor.ko aw32257_charger.ko charger-manager.ko tran_charger.ko musb_hdrc.ko musb_sprd.ko"
-
- 
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery
-
-PRODUCT_PACKAGES += \
-    bootctrl \
-    bootctrl.recovery 
-
 
 PRODUCT_PACKAGES += \
     bootctrl.ums9230
-    
-ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
+PRODUCT_PACKAGES += \
+    bootctrl.ums9230 \
+    libgptutils \
+    libz \
+    libcutils
 
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
-PRODUCT_PACKAGES_DEBUG += \
-    bootctrl.ums9230
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
@@ -55,19 +60,14 @@ PRODUCT_PACKAGES += \
     update_verifier \
     update_engine_sideload
 
-
-# Dynamic partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# A/B support
-AB_OTA_UPDATER := true
-
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 31
 PRODUCT_SHIPPING_API_LEVEL := 31
 # A/B
 
 
+
+
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl \
+    android.hardware.fastboot@1.0-impl-mock \
     fastbootd
