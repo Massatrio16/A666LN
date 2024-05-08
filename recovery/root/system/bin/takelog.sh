cd2ceb64d8bd236877fcdd354863be88242e3a35
Massatrio16 <146039181+Massatrio16@users.noreply.github.com>
2024-05-09 05:34:39 +0700

    Update takelog.sh

--- recovery/root/system/bin/takelog.sh
+++ recovery/root/system/bin/takelog.sh
@@ -1,16 +1,20 @@
 ##### Ke folder data ######
+
+mount /data
+mount /cache
+
 logcat -d > /data/log.txt 
 dmesg > /data/dmesg.txt
-cp /tmp/recovery.log /data/
+cp -r /tmp/recovery.log /data
 
 ##### ke folder cache ####
 logcat -d > /cache/log.txt 
 dmesg > /cache/dmesg.txt
-cp /tmp/recovery.log /cache/
+cp -r /tmp/recovery.log /cache
 
 
- cp /sys/fs/pstore > /data
- cp /proc/last_kmsg > /data
+ cp /sys/fs/pstore /data
+ cp /proc/last_kmsg  /data
  
- cp /sys/fs/pstore > /cache
- cp /proc/last_kmsg > /cache
+ cp /sys/fs/pstore /cache
+ cp /proc/last_kmsg /cache
