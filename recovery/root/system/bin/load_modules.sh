c871ab34d186f525ff21ec747728c868422117df
Massatrio16 <dimassetosatrio@gmail.com>
2024-09-10 17:56:19 +0700

    update

--- recovery/root/system/bin/load_modules.sh
+++ recovery/root/system/bin/load_modules.sh
@@ -12,21 +12,10 @@ load_panel()
 	insmod /lib/modules/fortsense_fp.ko
 	insmod /lib/modules/sc27xx_tsensor_thermal.ko
 	insmod /lib/modules/tran_gesture.ko
-	insmod /lib/chipone_icnl99xx.ko
-	insmod /lib/extcon-usb-gpio.ko
-	insmod /lib/gpio-eic-sprd.ko
-	insmod /lib/gpio-pmic-eic-sprd.ko
-	
 }
 
 
 load_panel
 wait 1
-mount /data
-mount -o remount, rw /data
-cp -r /tmp/recovery.log /data/
-cp -r /tmp/recovery.log /cache
-logcat -d > /data/log_reco.txt
-logcat -d > /cache/log_reco.txt
 setprop modules.loaded 1
 exit 0
