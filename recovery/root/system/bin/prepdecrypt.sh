28125a0bc29e52eb010e7757b15c4d13ef47b026
Massatrio16 <146039181+Massatrio16@users.noreply.github.com>
2024-09-11 17:20:21 +0700

    Update prepdecrypt.sh

--- recovery/root/system/bin/prepdecrypt.sh
+++ recovery/root/system/bin/prepdecrypt.sh
@@ -1,4 +1,9 @@
 #!/sbin/sh
 # We are in android 13
   setprop crypto.ready 1
+cp -r /tmp/recovery.log /cache
+cp -r /tmp/recovery.log /data
+logcat -d > /data/logcat_tw.txt
+logcat -d > /cache/logcat_tw.txt
+
 
